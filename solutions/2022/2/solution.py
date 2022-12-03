0b8d5a9fdc77b3487edea436cc19e19afe3dd16e
Ryan Tam <ryantam626@gmail.com>
2022-12-03 11:48:45 +0000

    fmt

--- solutions/2022/2/solution.py
+++ solutions/2022/2/solution.py
@@ -7,9 +7,9 @@ lines = read_input(YEAR, DAY)
 
 
 def gen():
-    mat = [[3,0,6],[6,3,0],[0,6,3]]
-    a = 'ABC'
-    b = 'XYZ'
+    mat = [[3, 0, 6], [6, 3, 0], [0, 6, 3]]
+    a = "ABC"
+    b = "XYZ"
     for line in lines:
         left, right = map(str.strip, line.split())
         yield mat[b.index(right)][a.index(left)] + b.index(right) + 1
@@ -19,11 +19,12 @@ print(sum(gen()))
 
 
 def gen2():
-    mat = [[3,1,2],[1,2,3],[2,3,1]]
-    a = 'ABC'
-    b = 'XYZ'
+    mat = [[3, 1, 2], [1, 2, 3], [2, 3, 1]]
+    a = "ABC"
+    b = "XYZ"
     for line in lines:
         left, right = map(str.strip, line.split())
         yield mat[b.index(right)][a.index(left)] + (b.index(right) * 3)
+
 
 print(sum(gen2()))
